a793c9ca316d04601d524924b9f3cd18e1cfc264
Gloria Ngbea <ngbeagloriajames@gmail.com>
2023-10-22 11:50:18 +0100

    second commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom'; // Fix the import statement
 import ErrorBoundary from './Components/ErrorBoundary';
 import App from './App';
 import './style.css'; // Import the CSS file
 
-
-const root = createRoot(document.getElementById('app'));
-
-root.render(
+ReactDOM.render(
   <StrictMode>
     <ErrorBoundary>
       <App />
     </ErrorBoundary>
-  </StrictMode>
+  </StrictMode>,
+  document.getElementById('app')
 );
